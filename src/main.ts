32e655bce3980523c4410657a43ae9c19594813a
jiho lee <optional.int@kakao.com>
2022-04-17 19:37:42 +0900

    fix: type errors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import {IRequest, IResponse} from "../index";
 import {Response} from './server'
 
 
-export default function handlerGen<Req, Res>(getter: (req: Req) => Res, mutate: (req: IRequest<Req>) => void) {
+export default function handlerGen<Req, Res>(getter: (req: Req) => Res, mutate: (req: Req) => void) {
     return function handler(req: IRequest<Req>): IResponse<Res> {
-        mutate(req);
+        mutate(req.get());
 
-        const found = getter(req);
+        const found = getter(req.get());
 
         return new Response(found);
     }
